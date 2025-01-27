9a7716a7649f1189eeab69a269b795df331699ae
Boukari Yameogo <yameogo.boukari@gmail.com>
2025-01-26 22:00:02 -0500

    fix

--- src/app/features/ui/hook.ts
+++ src/app/features/ui/hook.ts
@@ -1,4 +1,4 @@
-import {increment, decrement, reset} from "@/app/features/ui/incSlice";
+import {increment, decrement, reset, incrementBy, decrementBy, resetBy} from "@/app/features/ui/incSlice";
 import {useAppDispatch, useAppSelector} from "@/app/hooks";
 
 export const useIncrement = () => {
@@ -8,4 +8,13 @@ export const useIncrement = () => {
     const dec = () => dispatch(decrement());
     const rst = () => dispatch(reset());
     return { incCount, inc, dec, rst };
+}
+
+export const useIncrementBy = () => {
+    const incByCount = useAppSelector((state) => state.countReducer.incByValue);
+    const dispatch = useAppDispatch();
+    const incBy = (value: number) => dispatch(incrementBy(value));
+    const decBy = (value: number) => dispatch(decrementBy(value));
+    const rstBy = () => dispatch(resetBy());
+    return { incByCount, incBy, decBy, rstBy };
 }
--- src/app/tutorial/page.tsx
+++ src/app/tutorial/page.tsx
@@ -1,21 +1,33 @@
 "use client";
 
-import {useIncrement} from "@/app/features/ui/hook";
+import {useIncrement, useIncrementBy} from "@/app/features/ui/hook";
 
 const Tutorial = ({title} : {title: string}) => {
 
     const {incCount, inc, dec, rst} = useIncrement();
+    const {incByCount, incBy, decBy, rstBy} = useIncrementBy();
 
     return (
-        <div className={"flex flex-col justify-between items-center gap-5"}>
-            <h1>{title}</h1>
-            <div className={"mb-5"}>{incCount}</div>
-            <div className={"flex justify-between items-center gap-5"}>
-                <button className="px-4 py-2 text-white bg-blue-500 rounded-md active:bg-blue-950 active:animate-pulse" onClick={inc}>Increment</button>
-                <button className="px-4 py-2 text-white bg-blue-500 rounded-md active:bg-blue-950 active:animate-pulse" onClick={dec}>Decrement</button>
-                <button className="px-4 py-2 text-white bg-blue-500 rounded-md active:bg-blue-950 active:animate-pulse" onClick={rst}>Reset</button>
+        <>
+            <div className={"flex flex-col justify-between items-center gap-5"}>
+                <h1>{title}</h1>
+                <div className={"mb-5"}>{incCount}</div>
+                <div className={"flex justify-between items-center gap-5"}>
+                    <button className="px-4 py-2 text-white bg-blue-500 rounded-md active:bg-blue-950 active:animate-pulse" onClick={inc}>Increment</button>
+                    <button className="px-4 py-2 text-white bg-blue-500 rounded-md active:bg-blue-950 active:animate-pulse" onClick={dec}>Decrement</button>
+                    <button className="px-4 py-2 text-white bg-blue-500 rounded-md active:bg-blue-950 active:animate-pulse" onClick={rst}>Reset</button>
+                </div>
             </div>
-        </div>
+            <div className={"flex flex-col justify-between items-center gap-5"}>
+                <h1>{title}</h1>
+                <div className={"mb-5"}>{incByCount}</div>
+                <div className={"flex justify-between items-center gap-5"}>
+                    <button className="px-4 py-2 text-white bg-blue-500 rounded-md active:bg-blue-950 active:animate-pulse" onClick={() => incBy(10)}>Increment</button>
+                    <button className="px-4 py-2 text-white bg-blue-500 rounded-md active:bg-blue-950 active:animate-pulse" onClick={() => decBy(10)}>Decrement</button>
+                    <button className="px-4 py-2 text-white bg-blue-500 rounded-md active:bg-blue-950 active:animate-pulse" onClick={rstBy}>Reset</button>
+                </div>
+            </div>
+        </>
     )
 }
 
